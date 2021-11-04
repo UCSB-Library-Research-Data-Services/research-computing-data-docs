f06f1cb8e04d9c808b0d34b1fcce83331fe418bc
Elda Schaffield <emscha@ucsb.edu>
2021-11-03 18:29:50 -0700

    SSOPS-2447: Validation for the Libraries Handler script.

--- scripts/composer/LibrariesHandler.php
+++ scripts/composer/LibrariesHandler.php
@@ -9,6 +9,7 @@ namespace DrupalProject\composer;
 
 use Composer\Script\Event;
 use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
 
 class LibrariesHandler
 {
@@ -26,12 +27,20 @@ class LibrariesHandler
         $oldNames = [0 => "cycle", 1 => "jquery-hoverintent"];
         $newNames = [0 => "jquery.cycle", 1 => "jquery.hoverIntent"];
 
-        // rename if file exist
+        // rename the libraries but check case it already exists
         foreach($oldNames as $key => $fromFilename) {
             $toFilename = $newNames[$key];
-            if (!$fs->exists($root . '/libraries/' . $toFilename)) {
+            if ($fs->exists($root . '/libraries/' . $fromFilename) & $fs->exists($root . '/libraries/' . $toFilename)) {
+                $fs->remove($root . '/libraries/' . $toFilename);
+                $event->getIO()->write("  - Successfully removed $toFilename");
                 $fs->rename($root . '/libraries/'. $fromFilename, $root . '/libraries/' . $toFilename);
-                $event->getIO()->write("renamed folders");
+                $event->getIO()->write("  - Renamed $fromFilename to $toFilename");
+            }
+            else {
+                if ($fs->exists($root . '/libraries/' . $fromFilename) & !$fs->exists($root . '/libraries/' . $toFilename)) {
+                    $fs->rename($root . '/libraries/'. $fromFilename, $root . '/libraries/' . $toFilename);
+                    $event->getIO()->write("  - Renamed $fromFilename to $toFilename");
+                }
             }
         }
     }
