70dccf7e8fb773fbe102e449a5ed5725ac5ad1bf
Yanwen <91364746+AdeDeepFishing@users.noreply.github.com>
2024-05-28 10:46:30 -0700

    Delete web directory

--- web/modules/custom/sist/ssis_events/js/event-blocks.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var eventBlocks = document.getElementsByClassName('event-blocks-article');
-    var width = eventBlocks[0].offsetWidth;
-
-    for (let i = 0; i < eventBlocks.length; i++) {
-        eventBlocks[i].style.height = width + 'px';
-    }
-}, false);
